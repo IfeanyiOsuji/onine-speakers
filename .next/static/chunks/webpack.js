66628a72e8ac250e6a8156df07f799f6134b8afb
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-09-14 09:18:45 +0100

    Moved theContextprovider down to layout component closest to where it will be utilized

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "15896162efc83397"; }
+/******/ 		__webpack_require__.h = function() { return "12a9a17fc64e11d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import { createContext, useState } from "react";
 import Layout from "./components/layout/Layout";
-import ThemeContextProvider from "./components/contexts/themeContext";
-//export const ThemeContext = createContext();
+
 const App = ({ url }) => {
-  //const [darkTheme, setDarkTheme] = useState(false);
-
-  //const toggleTheme = () => setDarkTheme(!darkTheme)
-  
-  // let value = {
-  //   darkTheme,
-
-  //   toggleTheme
-  // }
-
-
 
   return (
-    <ThemeContextProvider>
+    
   <Layout url={url} />
-  </ThemeContextProvider>
+  
   )
 };
 
