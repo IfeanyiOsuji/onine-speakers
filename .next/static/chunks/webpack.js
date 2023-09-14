9a58d21a91f0fd5296480b62209983d9d6938262
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-09-14 09:26:49 +0100

    extracted state management from context provider

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12a9a17fc64e11d5"; }
+/******/ 		__webpack_require__.h = function() { return "b219c839fcd83c6f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/contexts/themeContext.js
+++ src/components/contexts/themeContext.js
@@ -1,15 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext} from "react";
+import { useTheme } from "../hooks/useTheme";
 
 export const ThemeContext = createContext({});
 
 const ThemeContextProvider = ({children, initialDarkTheme = false})=>{
-    const [darkTheme, setDarkTheme] = useState(initialDarkTheme);
-
-    const toggleTheme = () => setDarkTheme(!darkTheme);
-    const value = {
-        darkTheme,
-        toggleTheme
-    }
+   
+    const value = useTheme();
 
 return(
     <ThemeContext.Provider value={value}>
